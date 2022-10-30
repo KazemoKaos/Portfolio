90b92d8bb32a0d017ad8a60ff605d1904d2a49a8
Tyler Nguyen <tylern41653@gmail.com>
2022-10-29 23:56:56 -0400

    recent

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,78 +1,72 @@
-import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import headerImg from "../assets/img/header-img.svg";
-import { ArrowRightCircle } from 'react-bootstrap-icons';
+import { ArrowRightCircle } from "react-bootstrap-icons";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
+import { useState, useEffect } from "react";
 
 export const Banner = () => {
-  const [loopNum, setLoopNum] = useState(0);
-  const [isDeleting, setIsDeleting] = useState(false);
-  const [text, setText] = useState('');
-  const [delta, setDelta] = useState(300 - Math.random() * 100);
-  const [index, setIndex] = useState(1);
-  const toRotate = [ "Software Engineer", "Unity Developer" ];
-  const period = 2000;
+    const [loopNum, setLoopNum] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const [text, setText] = useState('');
+    const [delta, setDelta] = useState(0);
+    const toRotate = ["Software Engineer", "Unity Developer"];
+    const period = 2000;
 
-  useEffect(() => {
-    let ticker = setInterval(() => {
-      tick();
-    }, delta);
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            tick();
+        }, delta)
 
-    return () => { clearInterval(ticker) };
-  }, [text])
+        return () => { clearInterval(ticker) };
+    }, [text])
 
-  const tick = () => {
-    let i = loopNum % toRotate.length;
-    let fullText = toRotate[i];
-    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+    const tick = () => {
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
 
-    setText(updatedText);
+        setText(updatedText);
 
-    if (isDeleting) {
-      setDelta(prevDelta => prevDelta / 2);
+        if (isDeleting) {
+            setDelta(prevDelta => prevDelta / 1000);
+        }
+
+        if (!isDeleting && updatedText === fullText) {
+            setIsDeleting(true);
+
+            setDelta(period);
+        } else if (isDeleting && updatedText === '') {
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            setDelta(100);
+        } 
+
     }
-
-    if (!isDeleting && updatedText === fullText) {
-      setIsDeleting(true);
-      setIndex(prevIndex => prevIndex - 1);
-      setDelta(period);
-    } else if (isDeleting && updatedText === '') {
-      setIsDeleting(false);
-      setLoopNum(loopNum + 1);
-      setIndex(1);
-      setDelta(500);
-    } else {
-      setIndex(prevIndex => prevIndex + 1);
-    }
-  }
-
-  return (
-    <section className="banner" id="home">
-      <Container>
-        <Row className="aligh-items-center">
-          <Col xs={12} md={6} xl={7}>
-            <TrackVisibility>
-              {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                <span className="tagline">Welcome to my Portfolio</span>
-                <h1>{`Hi, I'm Tyler Nguyen`} </h1>
-                <h1>{`< `}<span className="wrap">{text}</span>{` >`}</h1>
-                  <p>Welcome! I'm currently a senior at University of New Haven majoring in Computer Science. I'm passionate about all computer related topics, especially programming and game development! Feel free to explore the site!</p>
-                  <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
-              </div>}
-            </TrackVisibility>
-          </Col>
-          <Col xs={12} md={6} xl={5}>
-            <TrackVisibility>
-              {({ isVisible }) =>
-                <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
-                  {/*<img src={headerImg} alt="Header Img"/>*/}
-                </div>}
-            </TrackVisibility>
-          </Col>
-        </Row>
-      </Container>
-    </section>
-  )
+    return (
+        <section className="banner" id="home">
+            <Container>
+                <Row className="align-items-center">
+                    <Col xs={12} md={6} xl={7}>
+                        <TrackVisibility once>
+                            {({ isVisible }) =>
+                                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                                    <span className="tagline">Welcome to my Portfolio</span>
+                                    <h1>{`Hi, I'm Tyler Nguyen! `}</h1>
+                                    <h1>{`< `}<span className="wrap">{text} {` >`}</span></h1>
+                                    <p>Welcome! I'm currently a senior at University of New Haven majoring in Computer Science. 
+                                      I'm passionate about all computer related topics, especially programming and game development! Feel free to explore the site!</p>
+                                    <a href="#connect" className="text-decoration-none"><button>Let's connect!<ArrowRightCircle size={25} /></button></a>
+                                </div>}
+                        </TrackVisibility>
+                    </Col>
+                    <TrackVisibility once>
+                        {({ isVisible }) =>
+                            <div className={isVisible ? "animate__animated animate__zoomIn" : "animate__animated animate__zoomOut"}>
+                                                {/*<img src={headerImg} alt="Header Img"/>*/}
+                            </div>}
+                    </TrackVisibility>
+                </Row>
+            </Container>
+        </section>
+    )
 }
